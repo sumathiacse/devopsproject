51b09d50b820b91eb5ae783d7e2305aebaabcea0
sumathiacse <133768248+sumathiacse@users.noreply.github.com>
2024-09-18 12:34:05 +0530

    Update testt.java

--- testt.java
+++ testt.java
@@ -3,5 +3,6 @@ class testt
     public static void main(String []args)
     {
         System.out.println("My First Java Program.");
+        System.out.println("welcome to git plugins.");
     }
 };
